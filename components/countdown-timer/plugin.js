e78a6b37b79266c4ab035f9118c69b28a5995599
Terence Flowers <tlflow33@gmail.com>
2015-11-02 20:30:02 -0500

    Added in widget factory
    setup widget factory and /components structure

---
+++ components/countdown-timer/plugin.js
@@ -0,0 +1,40 @@
+$.widget( "COP.timer", {
+
+    // Default options.
+    options: {
+        param1: "foo",
+        param2: "bar",
+        param3: "baz"
+    },
+
+    _create: function() {
+      // get next Sunday
+      var nextSunday = moment().day(7).format("YYYY-MM-DDT11:00:00Z");
+
+      // make it a moment object again
+      var event = moment(nextSunday);
+
+      // get current time/date
+      var current = moment();
+
+      // get difference between event and current
+      var diffTime = event.diff(current);
+
+      // let moment.js make the duration out of the timestamp
+      var duration = moment.duration(diffTime, 'milliseconds', true);
+
+      // set interval of timer to milliseconds
+      var interval = 1000;
+
+      setInterval(function(){
+        duration = moment.duration(duration - interval, 'milliseconds');
+        $('#clock').html(
+            "<div class=\'days cell\'>"+duration.days()+"<span>days</span></div>" +
+            "<div class=\'hours cell\'>"+duration.hours()+"<span>hours</span></div>" +
+            "<div class=\'mins cell\'>"+duration.minutes()+"<span>mins</span></div>" +
+            "<div class=\'secs cell\'>"+duration.seconds()+"<span>secs</span></div>")
+      }, interval);
+
+    }
+ 
+});
