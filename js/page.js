9d612a079c4843c2370e15cc0197a1b99e677a7d
Terence Flowers <tlflow33@gmail.com>
2015-11-06 20:41:41 -0500

    global navigation begins
    started messing around with global nav on small devices

--- js/page.js
+++ js/page.js
@@ -4,7 +4,26 @@ $(document).ready(function(){
     dots: true,
     infinite: true,
     speed: 500,
-    fade: true,
     cssEase: 'linear'
   });
+
+  // vision, purpose and belief buttons
+
+  $('body').on('click', '#about button', function(evt){
+    evt.preventDefault();
+
+    var sectiontitle = $(this).data( 'subtitle' );
+    $('#'+sectiontitle).toggleClass( 'active' );
+  });
+
+  // menu navigation
+
+  $( 'body').on( 'click', '#menu', function(evt){
+    evt.preventDefault();
+
+    var menubutton = $(this),
+        menuscreen = $('#global');
+
+    menuscreen.toggleClass('active');    
+  })
 });
