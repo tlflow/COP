28117e09d8ba239d05a001d69fd90d9b77d5cbe8
Terence Flowers <tlflow33@gmail.com>
2015-11-13 16:34:26 -0500

    Formspree forms
    Adding formspree form and svg logo

--- js/libraries.build.js
+++ js/libraries.build.js
@@ -1,5 +1,6 @@
 //@prepros-append ../bower_components/jquery/dist/jquery.min.js
 //@prepros-append ../bower_components/jquery-ui/ui/widget.js
+//@prepros-append ../bower_components/skrollr/dist/skrollr.min.js
 //@prepros-append ../bower_components/enquire/dist/enquire.min.js
 //@prepros-append ../bower_components/slick-carousel/slick/slick.min.js
 //@prepros-append ../bower_components/moment/min/moment.min.js
