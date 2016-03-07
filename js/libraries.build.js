312efc599964ecf90493d08809fce62fa2d7d012
Terence Flowers <tlflow33@gmail.com>
2016-03-07 14:00:03 -0500

    Integrated Wow.js
    Animate.css and Wow.Js to make scrolling page animations easier for now.
    Maybe in time we'll eliminate and just do it from scratch.

--- js/libraries.build.js
+++ js/libraries.build.js
@@ -1,5 +1,6 @@
 //@prepros-append ../bower_components/jquery/dist/jquery.min.js
 //@prepros-append ../bower_components/jquery-ui/ui/widget.js
+//@prepros-append ../bower_components/wow/dist/wow.min.js
 //@prepros-append ../bower_components/foundation-sites/dist/foundation.min.js
 //@prepros-append ../bower_components/moment/min/moment.min.js
 //@prepros-append ../bower_components/owlcarousel/owl-carousel/owl.carousel.min.js
