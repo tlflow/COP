62d11173a8fdd0edbb13399b84fde15780306f76
Terence Flowers <tlflow33@gmail.com>
2016-03-07 20:11:58 -0500

    changed order of files in libraries.js
    reordered the files so that they would be able to run effectively.
    Before they were just plastered in my html.

--- js/libraries.build.js
+++ js/libraries.build.js
@@ -1,7 +1,14 @@
 //@prepros-append ../bower_components/jquery/dist/jquery.min.js
 //@prepros-append ../bower_components/jquery-ui/ui/widget.js
+
 //@prepros-append ../bower_components/wow/dist/wow.min.js
-//@prepros-append ../bower_components/foundation-sites/dist/foundation.min.js
+
 //@prepros-append ../bower_components/moment/min/moment.min.js
+
+//@prepros-append ../bower_components/foundation-sites/dist/foundation.min.js
+
 //@prepros-append ../bower_components/owlcarousel/owl-carousel/owl.carousel.min.js
-//@prepros-append ../bower_components/scrollmagic/scrollmagic/minified/plugins/jquery.ScrollMagic.min.js
+
+//@prepros-append lib/greensock/TweenMax.min.js
+//@prepros-append ../bower_components/scrollmagic/scrollmagic/minified/ScrollMagic.min.js
+//@prepros-append ../bower_components/scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js
