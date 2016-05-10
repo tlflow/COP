0eb75f01456eb2ef1261d688e372de4229617dc3
Terence Flowers <tlflow33@gmail.com>
2016-05-10 02:08:32 -0400

    Re-organizing files for distribution
    now CSS/JS and nunjucks all publish out to /dist folder

--- js/component.build.js
+++ js/component.build.js
@@ -2,3 +2,5 @@
 //@prepros-append ../components/countdown-timer/plugin.js
 /* Page Navigation */
 //@prepros-append ../components/pagenav/plugin.js
+/* Connect Form */
+//@prepros-append ../components/connect-form/plugin.js
---
+++ js/debug.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  console.log('%c Current Browser Breakpoint: ', 'color: #bada55', Foundation.MediaQuery.current);
+}
