04452a12e7168e1cc4708143251fe168402e98ba
Terence Flowers <tlflow33@gmail.com>
2016-06-02 01:04:34 -0400

    Updates to content
    Cleaning up content nunjucks. Prep work for real content.

--- js/homepage.js
+++ js/homepage.js
@@ -1,7 +1,58 @@
 $(function(){
   $(document).foundation();
   $('#sunday-clock').timer();
-  // $(".owl-carousel").owlCarousel(); // uncomment for carousel eventually
+
+  //////////////////////////////////
+  /// homepage form submission /////
+  //////////////////////////////////
+
+  $('form').submit( function( event ) {
+
+    event.preventDefault();
+
+    var trimmedComment = $.trim( $('textarea').val() );
+
+    var formData = {
+
+        'name'      : $('input[name=name]').val(),
+        'email'     : $('input[name=email]').val(),
+        'phone'     : $('input[name=phone]').val(),
+        'status'    : $('select[name=status]').val(),
+        'moreinfo'  : $('input[name=moreinfo]').is(':checked'),
+        'joingroup' : $('input[name=joingroup]').is(':checked'),
+        'comment'   : trimmedComment
+
+    };
+
+    $.ajax({
+
+      type     : 'POST',
+      url      : 'process_home-form.php',
+      data     : formData,
+      dataType : 'json',
+      encode   : true
+
+    }).done( function( data ) {
+
+      console.log( data );
+
+      if ( ! data.success ) {
+
+        // if ( data.errors.name ) {
+        //
+        // }
+        console.log("show error messages");
+
+      } else {
+
+        // ITS ALL GOOD! SHOW SUCCESS
+        $('form').append( '<div class="alert alert-success">' + data.message + '</div>' )
+
+      }
+
+
+    });
+
 
   //////////////////////////////////////////
   /// fading masthead layer on homepage ////
