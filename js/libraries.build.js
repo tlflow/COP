6d7c00f0ab457ff0e840f5c0db61cae3789fdcdd
Terence Flowers <tlflow33@gmail.com>
2016-06-09 00:15:25 -0400

    Re-introducing Owl Carousel to the mix

--- js/libraries.build.js
+++ js/libraries.build.js
@@ -1,4 +1,5 @@
 //@prepros-append ../bower_components/jquery/dist/jquery.min.js
+
 //@prepros-append ../bower_components/jquery-ui/ui/widget.js
 
 //@prepros-append ../bower_components/wow/dist/wow.min.js
@@ -6,5 +7,7 @@
 //@prepros-append ../bower_components/lodash/dist/lodash.min.js
 
 //@prepros-append ../bower_components/moment/min/moment.min.js
+
+//@prepros-append ../bower_components/owlcarousel/owl-carousel/owl.carousel.min.js
 
 //@prepros-append ../bower_components/foundation-sites/dist/foundation.min.js
--- js/subpage.js
+++ js/subpage.js
@@ -2,6 +2,28 @@ $(document).ready(function(){
   $(document).foundation();
   // $(".owl-carousel").owlCarousel(); // uncomment for carousel eventually
 
+  if ($('#page.about').length==1) {
+
+    $('.owl-carousel').owlCarousel({
+        margin:10,
+        autoplay: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5,
+                nav: true,
+                loop: true
+            }
+        }
+    });
+
+  }
+
   // copyright
   $("#copyright").prepend("&copy;" + moment().format('YYYY')+ " ");
 });
