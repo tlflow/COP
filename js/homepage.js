c3cf9bbd1b3b4da0ed33f7615755937bd01a0624
Terence Flowers <tlflow33@gmail.com>
2017-03-05 16:15:30 -0500

    Removed sliding box theme from homepage
    going to do that a little differently. moving box kinda messes with the slider when you slide the page up and down.

--- js/homepage.js
+++ js/homepage.js
@@ -67,29 +67,29 @@ $(function(){
   /// fading masthead layer on homepage ////
   //////////////////////////////////////////
 
-  var fadingMasthead = {
-
-  	fadeLayer: function() {
-  		var $_topImage, fadeStart, fadeUntil, opacity;
-
-  		$_topImage  = $('.image-1'),
-  		fadeStart = $_topImage.offset().top,
-  		fadeUntil = $_topImage.height();
-  		opacity = 0;
-
-  		if ( scrollPos<=fadeStart ) {
-  			opacity = 1;
-  		} else if( scrollPos<=fadeUntil ) {
-  			opacity = 1-scrollPos/fadeUntil;
-  		}
-
-  		$_topImage.css('opacity', opacity);
-
-  	},
-
-  	_init: function( scrollPos ) {
-  		this.fadeLayer( scrollPos );
-  	}
-  };
+  // var fadingMasthead = {
+  //
+  // 	fadeLayer: function() {
+  // 		var $_topImage, fadeStart, fadeUntil, opacity;
+  //
+  // 		$_topImage  = $('.image-1'),
+  // 		fadeStart = $_topImage.offset().top,
+  // 		fadeUntil = $_topImage.height();
+  // 		opacity = 0;
+  //
+  // 		if ( scrollPos<=fadeStart ) {
+  // 			opacity = 1;
+  // 		} else if( scrollPos<=fadeUntil ) {
+  // 			opacity = 1-scrollPos/fadeUntil;
+  // 		}
+  //
+  // 		$_topImage.css('opacity', opacity);
+  //
+  // 	},
+  //
+  // 	_init: function( scrollPos ) {
+  // 		this.fadeLayer( scrollPos );
+  // 	}
+  // };
 
 });
